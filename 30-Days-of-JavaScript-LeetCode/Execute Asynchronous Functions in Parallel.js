022538040ee775bfd52a032e3470b00c221bdb3c
gamal2001 <kmhamak@gmail.com>
2023-08-11 07:07:22 +0300

    add Execute Asynchronous Functions in Parallel  solution

---
+++ 30-Days-of-JavaScript-LeetCode/Execute Asynchronous Functions in Parallel.js
@@ -0,0 +1,40 @@
+// declare an async function named promiseAll that takes an array of functions as a parameter
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = async function (functions) {
+  // return a new promise object
+  return new Promise((resolve, reject) => {
+    // declare an empty array to store the results
+    const results = [];
+    // declare a counter to track the number of completed functions
+    let completed = 0;
+
+    // check if the parameter array is empty
+    if (!functions.length) {
+      // resolve the promise with the empty array
+      resolve(results);
+    }
+
+    // loop through the parameter array
+    functions.forEach((asyncFunc, index) => {
+      // call each function and expect it to return a promise
+      asyncFunc()
+        // attach a then handler to the promise
+        .then((result) => {
+          // store the result in the corresponding index of the results array
+          results[index] = result;
+          // increment the counter by one
+          completed++;
+          // check if all functions have completed
+          if (completed === functions.length) {
+            // resolve the promise with the results array
+            resolve(results);
+          }
+        })
+        // attach a catch handler to the promise
+        .catch(reject); // reject the promise with the error
+    });
+  });
+};
