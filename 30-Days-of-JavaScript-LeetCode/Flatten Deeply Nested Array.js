8f304e5f29133a52131382ce76f8836ccd451ba0
gamal2001 <kmhamak@gmail.com>
2023-08-18 12:03:32 +0300

    add flatten deeply nested array

---
+++ 30-Days-of-JavaScript-LeetCode/Flatten Deeply Nested Array.js
@@ -0,0 +1,17 @@
+var flat = function (arr, n) {
+    if (n === 0) {
+        return arr;
+    }
+    
+    let answer = [];
+    
+    arr.forEach(element => {
+        if (n > 0 && Array.isArray(element)) {
+            answer.push(...flat(element, n - 1));
+        } else {
+            answer.push(element);
+        }
+    });
+    
+    return answer;
+};
